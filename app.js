749acd29430599458778f101a3f189ddb8b99b73
hauze-v <hauze_v@protonmail.com>
2020-08-05 16:30:58 -0400

    HTML and CSS portion complete for count-down-timer

---
+++ app.js
@@ -0,0 +1,15 @@
+// DOM references
+const timerDayEl = document.querySelector(".timer__day")
+const timerHourEl = document.querySelector(".timer__hour")
+const timerMinEl = document.querySelector(".timer__min")
+const timerSecEl = document.querySelector(".timer__sec")
+
+// There are two dates. Now and the end date
+function getTimeDifference(start, end) {
+
+}
+
+const startDate = new Date(); // this is the current time, right now
+const targetDate = new Date(`September 13, 2020 0:00:00`); // target date
+
+getTimeDifference()
